963850f8fd909ab76d12734cd09c163d35a212a7
Joseph Lee <facebooklee52@gmail.com>
2025-01-18 16:01:31 +0800

    Rebrand

--- src/functions/helpers.js
+++ src/functions/helpers.js
@@ -4,6 +4,6 @@ import { AppContext } from "../App";
 
 export default function titleHelper(title, docTitle=null) {
     const { setTitle } = useContext(AppContext);
-    document.title = (docTitle ? docTitle : title) + " | MidoriSKY";
+    document.title = (docTitle ? docTitle : title) + " | ReclaimIt";
     setTitle(title);
 }
